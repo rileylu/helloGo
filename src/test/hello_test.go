98b38b4faef7f9f082ae07545ead3095725ad39e
luri <riley.lu@oocl.com>
2019-02-20 17:06:22 +0800

    commit

--- src/test/hello_test.go
+++ src/test/hello_test.go
@@ -13,7 +13,7 @@ func TestSayHello1(t *testing.T) {
 
 func TestSayHello2(t *testing.T) {
 	user := "777"
-	if str := SayHello2(user); str != "Hello, 777" {
+	if str := SayHello2(user); str != "Hello,777" {
 		t.Error("SayHello2 is failed")
 	} else {
 		t.Log("SayHello2 is ok")
